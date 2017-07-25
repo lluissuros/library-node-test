c4567bd67e147c15933a2f348165a7bdee552712
Lluis Suros <lluissuros@gmail.com>
2017-07-26 01:05:39 +0200

    feat(app.js): Add env var and linter happy

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express = require('express');
 
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+    res.send('hello world');
 });
 
-app.get('/books', (req, res) => {
-  res.send('hello books');
+app.get('/books', (req, res) =>  {
+    res.send('hello books');
 });
 
 app.listen(port, (err) => {
-  console.log('listening on port ' + port);
+    console.log('listening on port ' + port);
 });
