a82f6c6e9a8b151193258a8f5090f335e2ead768
Lluis Suros <lluissuros@gmail.com>
2017-07-31 18:13:53 +0200

    Add .ejs as Templating Engine
     * Rename index.html for index.ejs
     * Add a for loop on the index.ejs
     * Modify app.js to use ejs as templating engine and provide some dynamic data

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+// app.use(express.static('src/views'));
+app.set('views', './src/views');
+//set view engine to ejs
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    // res.send('hello world');
+    res.render('index', {title: 'hello', list: ['a', 'b']});
 });
 
 app.get('/books', (req, res) =>  {
