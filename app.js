a892631ea5da30c853cc8aeaa1c466807999e284
Lluis Suros <lluissuros@gmail.com>
2017-08-01 15:49:32 +0200

    Add express Router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 // app.use(express.static('src/views'));
@@ -9,14 +10,31 @@ app.set('views', './src/views');
 //set view engine to ejs
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get((req, res) => {
+        res.send('hello books from router');
+    });
+bookRouter.route('/single')
+    .get((req, res) => {
+        res.send('hello books/single from router');
+    });
+
+app.use('/Books', bookRouter);
+
 app.get('/', (req, res) => {
     // res.send('hello world');
-    res.render('index', {title: 'hello', list: ['a', 'b']});
+    res.render('index', {
+        title: 'hello',
+        nav: [
+            {Link:'/Books',text:'Books'},
+            {Link:'/Authors',text:'Authors'},
+        ]
+    });
 });
 
-app.get('/books', (req, res) =>  {
-    res.send('hello books');
-});
+// app.get('/books', (req, res) =>  {
+//     res.send('hello books');
+// });
 
 app.listen(port, (err) => {
     console.log('listening on port ' + port);
